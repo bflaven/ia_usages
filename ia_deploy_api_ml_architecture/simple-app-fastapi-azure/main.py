aa31fb2478df7e97b995b60a7d530334eea24075
Bruno Flaven <bflaven@gmail.com>
2023-10-29 10:26:13 +0100

    updates for YT

--- ia_deploy_api_ml_architecture/simple-app-fastapi-azure/main.py
+++ ia_deploy_api_ml_architecture/simple-app-fastapi-azure/main.py
@@ -3,20 +3,18 @@
 #
 from fastapi import FastAPI, File, status
 from fastapi.responses import RedirectResponse
-# from fastapi.responses import StreamingResponse
-# from fastapi.responses import FileResponse
-# from fastapi.middleware.cors import CORSMiddleware
-# from fastapi.exceptions import HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import HTTPException
  
 app = FastAPI()
 
 # home
 @app.get("/")
 async def home():
-    return RedirectResponse("/docs")
+    # return RedirectResponse("/docs")
+    return {'Azure FastAPI test api': 'It is running'}
 
-@app.get("/healthcheck")
-async def perform_healthcheck():
-    return {'Youtube Again and Again Test Again Azure FastAPI test api': 'It is running and it is OK.'}
     
     
