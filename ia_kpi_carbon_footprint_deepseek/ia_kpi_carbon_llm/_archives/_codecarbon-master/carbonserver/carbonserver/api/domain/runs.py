e8a452bca15b383835b913dd2c9ac0ec18e3740c
Bruno Flaven <bflaven@gmail.com>
2025-02-16 06:50:39 +0100

    update files

--- ia_kpi_carbon_footprint_deepseek/ia_kpi_carbon_llm/_archives/_codecarbon-master/carbonserver/carbonserver/api/domain/runs.py
+++
@@ -1,9 +0,0 @@
-import abc
-
-from carbonserver.api import schemas
-
-
-class Runs(abc.ABC):
-    @abc.abstractmethod
-    def add_run(self, run: schemas.RunCreate):
-        raise NotImplementedError
