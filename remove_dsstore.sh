75435c4778d8176f6e702425e0a6d1ef2676a979
Bruno Flaven <brunoflaven@MacBook-Pro-de-APCLOCAL-2.local>
2025-11-24 14:03:40 +0100

    Commit: add new files (2025-11-24 14:03:40)

--- remove_dsstore.sh
+++ remove_dsstore.sh
@@ -14,14 +14,17 @@
 # =========================================================
 
 #!/bin/bash
+# Set your GitHub project directory
+TARGET_DIR="/Users/brunoflaven/Documents/03_git/ia_usages"
 
-# Set your target GitHub directory
-GIT_DIR="/Users/brunoflaven/Documents/03_git/ia_usages"
+# Navigate to the target directory
+cd "$TARGET_DIR"
 
-# Recursively find and delete all ".DS_Store" files
-find "$GIT_DIR" -type f -name ".DS_Store" -delete
+# Recursively find and delete all .DS_Store files
+# find . -name ".DS_Store" -type f -delete
+find . -type f \( -name ".DS_Store" -o -name "._.DS_Store" \) -delete
 
-echo "All .DS_Store files removed from $GIT_DIR and its subdirectories."
+echo "All .DS_Store files removed from $TARGET_DIR and its subdirectories."
 
 
 
